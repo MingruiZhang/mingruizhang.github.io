fcc3c15f371d2919cc9eddaa78a25721a29da550
Mingrui Zhang <mingrui.zhang@airbnb.com>
2020-02-16 15:04:09 +0800

    save

--- webpack.config.js
+++ webpack.config.js
@@ -2,13 +2,10 @@ const webpack = require("webpack");
 const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-// ExtractTextPlugin doesn't work with Webpack 4
-// const ExtractTextPlugin = require("extract-text-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const CleanWebpackPlugin = require('clean-webpack-plugin');
+const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 const rootPath = path.join(__dirname, "./");
-const distPath = path.join(__dirname, "./dist");
 const srcPath = path.join(__dirname, "./src");
 
 const rules = [
@@ -19,41 +16,43 @@ const rules = [
   },
   {
     test: /\.css$/,
-    use: [
-      MiniCssExtractPlugin.loader,
-      "css-loader"
-    ]
+    use: [MiniCssExtractPlugin.loader, "css-loader"]
   },
   { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
 ];
 
-const plugins = [
-  new CleanWebpackPlugin(['dist']),
-  new webpack.HotModuleReplacementPlugin(),
-  new webpack.NamedModulesPlugin(),
+const commonPlugins = [
   new HtmlWebpackPlugin({ template: path.join(srcPath, "index.html") }),
   new MiniCssExtractPlugin({ filename: "dist/css/[name].css" })
 ];
 
-module.exports = {
-  entry: path.join(srcPath, "index.tsx"),
-  mode: 'production',
-  output: {
-    path: rootPath,
-    // HTML need to be under root for github pages
-    publicPath: '/',
-    filename: "dist/js/[name].js"
-  },
-  module: {
-    rules
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
-  resolve: {
-    extensions: [".ts", ".tsx", ".js", ".json"]
-  },
-  plugins
+const devPlugin = [new webpack.HotModuleReplacementPlugin()];
+
+const prodPlugin = [new CleanWebpackPlugin(["dist"])];
+
+module.exports = () => {
+  const isDev = process.env.NODE_ENV === "dev";
+  const plugins = isDev ? [...commonPlugins, ...devPlugin] : [...commonPlugins, ...prodPlugin];
+  return {
+    entry: path.join(srcPath, "index.tsx"),
+    mode: isDev ? "development" : "production",
+    output: {
+      path: rootPath,
+      // HTML need to be under root for github pages
+      publicPath: "/",
+      filename: "dist/js/[name].js"
+    },
+    module: {
+      rules
+    },
+    devServer: {
+      historyApiFallback: true
+    },
+    // Enable sourcemaps for debugging webpack's output.
+    devtool: "source-map",
+    resolve: {
+      extensions: [".ts", ".tsx", ".js", ".json"]
+    },
+    plugins
+  };
 };
