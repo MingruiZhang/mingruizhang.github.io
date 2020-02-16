5a4b9bd5ef9d3f4cdf412d60dda651de23739cc5
Mingrui Zhang <mingrui.zhang@airbnb.com>
2020-02-17 01:33:54 +0800

    Weekly - 20.07

--- tools/txt2json.js
+++ tools/txt2json.js
@@ -9,11 +9,14 @@ fs.readdir(articlePath, (err, filenames) => {
   filenames.forEach((filename, index) => {
     if (filename.match(/\.txt/)) {
       const content = fs.readFileSync(path.join(articlePath, filename), "utf8");
-      const [time, title] = filename.split(" - ").length > 1 ? filename.split(" - ") : ['', filename];
+      const [time, ...title] = filename.split(" - ");
+      // Below not working because 置顶 · 杂记本
+      // const fileNameRegex = /([0-9]{4}.[0-9]{2}.[0-9]{2}) - (.*).txt/
+      // const [_, time, title] = filename.match(fileNameRegex);
       const articleData = {
         id: index.toString(), // TODO: A better way is to Hash the title
         content,
-        title: title.replace(/\.[^/.]+$/, ""), // remove .txt extension
+        title: title.join(" - ").replace(/\.[^/.]+$/, ""), // remove .txt extension
         time
       };
       // TODO: Figure out how to order properly
