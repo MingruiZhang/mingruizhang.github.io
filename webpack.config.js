3486fd24bdfbee8073d63d5e9c9b199563581f34
Mingrui Zhang <mingrui.zhang@airbnb.com>
2020-02-16 16:37:43 +0800

    save

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,13 @@ const webpack = require("webpack");
 const path = require("path");
 
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const CleanWebpackPlugin = require("clean-webpack-plugin");
 
 const rootPath = path.join(__dirname, "./");
 const srcPath = path.join(__dirname, "./src");
 
-const rules = [
-  {
-    test: /\.tsx?$/,
-    use: "babel-loader",
-    exclude: /node_modules/
-  },
-  {
-    test: /\.css$/,
-    use: [MiniCssExtractPlugin.loader, "css-loader"]
-  },
-  { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
-];
-
-const commonPlugins = [
-  new HtmlWebpackPlugin({ template: path.join(srcPath, "index.html") }),
-  new MiniCssExtractPlugin({ filename: "dist/css/[name].css" })
-];
-
+const commonPlugins = [new HtmlWebpackPlugin({ template: path.join(srcPath, "index.html") })];
 const devPlugin = [new webpack.HotModuleReplacementPlugin()];
-
 const prodPlugin = [new CleanWebpackPlugin(["dist"])];
 
 module.exports = () => {
@@ -43,7 +24,14 @@ module.exports = () => {
       filename: "dist/js/[name].js"
     },
     module: {
-      rules
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "babel-loader",
+          exclude: /node_modules/
+        },
+        { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
+      ]
     },
     devServer: {
       historyApiFallback: true
